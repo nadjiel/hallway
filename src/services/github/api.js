5ae69cc97288ab242045d1a67d756b4814701ab0
Daniel O Sousa <oliveira.daaaaniel@gmail.com>
2023-07-30 12:47:02 -0300

    Refactoring Github's connectivity API code

--- src/services/github/api.js
+++ src/services/github/api.js
@@ -1,9 +1,11 @@
 import { get } from "../../utils";
 
+const api = "https://api.github.com";
+
 export async function getUser(user) {
-  return await get(`https://api.github.com/users/${user}`);
+  return await get(`${api}/users/${user}`);
 }
 
 export async function getRepos(user) {
-  return await get(`https://api.github.com/users/${user}/repos`);
+  return await get(`${api}/users/${user}/repos`);
 }
